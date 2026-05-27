332ea07233bf374c31663bfe8c8a6077e220bd16
vxartshop-droid <vxart.shop@gmail.com>
2026-05-27 10:46:41 -0300

    Create links.js

---
+++ links.js
@@ -0,0 +1,23 @@
+const links = {
+
+  "calca-jogger": "https://meli.la/29JRYe4",
+
+  "fone-bluetooth": "COLE_AQUI_LINK_AFILIADO",
+
+  "smartwatch": "COLE_AQUI_LINK_AFILIADO",
+
+  "capinha-a07": "COLE_AQUI_LINK_AFILIADO",
+
+  "ring-light": "COLE_AQUI_LINK_AFILIADO",
+
+  "mouse-gamer": "COLE_AQUI_LINK_AFILIADO",
+
+  "teclado-rgb": "COLE_AQUI_LINK_AFILIADO",
+
+  "airfryer": "COLE_AQUI_LINK_AFILIADO",
+
+  "ventilador": "COLE_AQUI_LINK_AFILIADO",
+
+  "cadeira-gamer": "COLE_AQUI_LINK_AFILIADO"
+
+};
